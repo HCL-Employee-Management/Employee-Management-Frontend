dbc512d3f8aa1edad9cc0d9169756e5f7cf43358
Munirathinam_E <munirathinam6724@gmail.com>
2026-02-24 23:13:16 +0530

    Initial added the stucture of an folder

---
+++ src/app/modules/leave/leave.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LeaveComponent } from './leave/leave.component';
+
+
+
+@NgModule({
+  declarations: [
+    LeaveComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LeaveModule { }
