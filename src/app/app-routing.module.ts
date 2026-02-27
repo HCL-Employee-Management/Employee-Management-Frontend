29c4575f9719e5c34056907e78a0fa30ec3b474f
Anusuba18 <anu18balu@gmail.com>
2026-02-27 10:55:15 +0530

    payroll service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { DummyLoginComponent } from './modules/dummy-login/dummy-login.component';
 import { AttendanceComponent } from './modules/attendance/attendance/attendance.component';
 import { LeaveComponent } from './modules/leave/leave/leave.component';
+import { DashboardComponent } from './modules/dashboard/dashboard/dashboard.component';
+import { PayrollComponent } from './modules/payroll/payroll/payroll.component';
 
 const routes: Routes = [
   { path: '', component: DummyLoginComponent },
   { path: 'attendance', component: AttendanceComponent },
-  { path: 'leave', component: LeaveComponent }
+  { path: 'leave', component: LeaveComponent },
+   { path: 'dashboard', component: DashboardComponent },
+  { path: 'payroll', component: PayrollComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/modules/payroll/payroll/payroll.component.ts
+++ src/app/modules/payroll/payroll/payroll.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PayrollService } from 'src/app/core/services/payroll.service';
 
 @Component({
   selector: 'app-payroll',
   templateUrl: './payroll.component.html',
   styleUrls: ['./payroll.component.css']
 })
-export class PayrollComponent {
+export class PayrollComponent implements OnInit {
+   payrollList: any[] = [];
 
+  constructor(private payrollService: PayrollService) {}
+
+  ngOnInit() {
+    this.loadPayroll();
+  }
+
+  loadPayroll() {
+    this.payrollService.getPayrollByEmployee(1)
+      .subscribe(data => {
+        this.payrollList = data;
+      });
+  }
 }
