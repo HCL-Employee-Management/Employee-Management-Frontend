38c25b1b433d170de24f15d8cdc0edbc11bee2fe
Anusuba18 <anu18balu@gmail.com>
2026-02-28 10:27:48 +0530

    updated payroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { AttendanceComponent } from './modules/attendance/attendance/attendance.component';
 import { DummyLoginComponent } from './modules/dummy-login/dummy-login.component';
-
+import { CommonModule } from '@angular/common';
 import { LeaveComponent } from './modules/leave/leave/leave.component';
 import { PayrollComponent } from './modules/payroll/payroll/payroll.component';
 import { DashboardComponent } from './modules/dashboard/dashboard/dashboard.component';
@@ -26,7 +26,8 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
